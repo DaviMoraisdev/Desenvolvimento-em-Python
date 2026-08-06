5da370b0a1ba17d29cc3249bba1f8c9342b10bf2
DaviMoraisdev <davimorais063@gmail.com>
2026-08-06 12:20:07 -0300

    feat: adiciona exercícios introdutórios de POO

---
+++ POO/instancia.py
@@ -0,0 +1,24 @@
+class Movie:
+    name = ""
+    yearLaunch = 0
+    includePlan = False
+    note = 0
+    durationMinutes = 0
+
+# Primeiro Filme #
+movie = Movie()
+movie.name = "A silent voice"
+movie.yearLaunch = 2016
+movie.includePlan = False
+movie.note = 10
+movie.durationMinutes = 130
+
+
+
+
+
+
+
+
+print("##Dados do Filme##")
+print(f"Nome do filme: {movie.name} \n Ano de Lançamento: {movie.yearLaunch} \n Duração: {movie.durationMinutes} minutos")
