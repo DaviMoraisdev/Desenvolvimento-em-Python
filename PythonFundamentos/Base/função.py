f1ca7f3e98b595586b8f253edabac86eb8700d21
Davi-design <137007200+Davi-design@users.noreply.github.com>
2026-08-20 22:28:08 -0300

    prática

---
+++ PythonFundamentos/Base/função.py
@@ -0,0 +1,16 @@
+def calcular_area_quadrado(lado):
+    return lado * lado 
+    print(f"Area: {area:.2f}") 
+
+calcular_area_quadrado(5)
+calcular_area_quadrado(10)
+calcular_area_quadrado(9)
+
+"""
+
+def nome_funcao(parametro_1, parametro_2):
+    Essa função soma dois números
+    resultado =parametro_1 + parametro_2
+    return resultado 
+
+""" 
